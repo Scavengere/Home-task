fd7a5b37d7f46398378c65310f97683251d37770
Scavengere <44781876+Scavengere@users.noreply.github.com>
2019-03-16 17:16:21 +0300

    Update main and Student classes
    Customize Student class using various types of statements and parameters

--- Task1/Student.cs
+++ Task1/Student.cs
@@ -4,22 +4,48 @@ namespace Task1
 {
     class Student
     {
-        public string nameOfStudent;
-        public int ageOfStudent;
-        public int numberOfSubject;
+        public string NameOfStudent { get; set; }
+        public int AgeOfStudent { get; set; }
+        public int NumberOfSubject { get; set; }
         public Mark[] Marks;
+ 
         public Student(string Name, int Age)
         {
-            nameOfStudent = Name;
-            ageOfStudent = Age;
+            NameOfStudent = Name;
+             AgeOfStudent = Age;
         }
         public void IntroductionOfSubjects()
         {
+            loop1:
             Console.Write("Enter the number of subject: ");
-            numberOfSubject = Convert.ToInt32(Console.ReadLine());
-            Marks = new Mark[numberOfSubject];
+            try
+            {
+                NumberOfSubject = Convert.ToInt32(Console.ReadLine());
+            }
+            catch(FormatException)
+            {
+                Console.Write("You entered a variable of the wrong type. Press any button to repeat...");
+                Console.ReadKey();
+                Console.Clear();
+                goto loop1;
+            }
+            if(NumberOfSubject > 5)
+            {
+                Console.Write("Students cannot study more than 5 subjects. Press any button to repeat...");
+                Console.ReadKey();
+                Console.Clear();
+                goto loop1;
+            }
+            if(NumberOfSubject < 1)
+            {
+                Console.Write("You have entered the number of items less than 1. Press any button to repeat...");
+                Console.ReadKey();
+                Console.Clear();
+                goto loop1;
+            }
+            Marks = new Mark[NumberOfSubject];
 
-            for (int i = 0; i < numberOfSubject; i++)
+            for (int i = 0; i < NumberOfSubject; i++)
             {
                 Marks[i] = new Mark();
                 Console.Write("Enter subject name: ");
@@ -30,8 +56,8 @@ namespace Task1
         }
         public static void OutputInformationOfStudent(Student student)
         {
-            Console.WriteLine($"Name - {student.nameOfStudent}\nAge - {student.ageOfStudent}");
-            for (int i = 0; i < student.numberOfSubject; i++)
+            Console.WriteLine($"Name - {student.NameOfStudent}\nAge - {student.AgeOfStudent}");
+            for (int i = 0; i < student.NumberOfSubject; i++)
             {
                 Console.WriteLine($"Subject - {student.Marks[i].nameOfSubject} ------ Mark - {student.Marks[i].mark}");
             }
@@ -39,15 +65,55 @@ namespace Task1
         public double GetAvgMark()
         {
             double avgMark = 0;
-            for (int i = 0; i < numberOfSubject; i++)
+            for (int i = 0; i < NumberOfSubject; i++)
                 avgMark += Marks[i].mark;
-            avgMark /= numberOfSubject;
+            avgMark /= NumberOfSubject;
             return avgMark;
         }
         public void ResetAllMarks()
         {
-            for (int i = 0; i < numberOfSubject; i++)
+            for (int i = 0; i < NumberOfSubject; i++)
                 Marks[i].mark = 0;
+        }
+        
+        static public void InputName(out string Name)
+        {
+            Console.Write("Enter name of student: ");
+            Name = Console.ReadLine();
+            Console.Clear();
+        }
+        static public void InputAge(out int Age)
+        {
+        loop1:
+            Console.Write("Enter age of student: ");
+            try
+            {
+                Age = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.Write("You entered a variable of the wrong type. Press any button to repeat...");
+                Console.ReadKey();
+                Console.Clear();
+                goto loop1;
+            }
+            if (Age > 0)
+            {
+                if (Age < 18)
+                {
+                    Console.WriteLine("Persons younger 18 are not allowed. Press any button to repeat...");
+                    Console.ReadKey();
+                    Console.Clear();
+                    goto loop1;
+                }
+            }
+            else
+            {
+                Console.WriteLine("You entered a negative age. Press any button to repeat...");
+                Console.ReadKey();
+                Console.Clear();
+                goto loop1;
+            }
         }
     }
 }

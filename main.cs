8d31487d61264e8ca6a40091d2de0541fc450a37
Scavengere <44781876+Scavengere@users.noreply.github.com>
2019-03-16 17:13:37 +0300

    Update main and Student classes
    Customize Student class using various types of statements and parameters

---
+++ main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task1
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            Student.InputName(out string Name);
+            Student.InputAge(out int Age);
+
+            Student first = new Student(Name, Age);
+            first.IntroductionOfSubjects();
+            Console.Clear();
+
+            Student.OutputInformationOfStudent(first);
+            Console.WriteLine($"\nAverage mark: {first.GetAvgMark()}\n");
+            first.ResetAllMarks();
+            Student.OutputInformationOfStudent(first);
+            Console.ReadKey();
+        }
+
+    }
+}

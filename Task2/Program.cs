39a812b7186cc16e29c4584b8a23cbb933eb4a5d
Scavengere <44781876+Scavengere@users.noreply.github.com>
2019-03-25 22:07:40 +0300

    Add files via upload

---
+++ Task2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InputNumberOfEmployees(out int numberOfEmployees);
+            Employee[] employees = new Employee[numberOfEmployees];
+            for (int i = 0; i < numberOfEmployees; i++)
+            {
+                InputInformation(out string name, out int salary);
+                employees[i] = new Employee(name, salary);
+            }
+            Console.Clear();
+            Console.WriteLine("Employees: ");
+            Employee.Output(employees);
+            Console.WriteLine("\nSorting by salary: ");
+            Employee.SortingBySalary(employees);
+            Employee.Output(employees);
+            Console.WriteLine("\nSorting by name: ");
+            Employee.SortingByName(employees);
+            Employee.Output(employees);
+            
+            Console.ReadKey();
+        }
+        static public void InputNumberOfEmployees(out int numberOfEmployees)
+        {
+            int temp = 0;
+            bool successfulInput = false;
+            while (successfulInput == false)
+            {
+                try
+                {
+                    Console.WriteLine("Enter the number of employees: ");
+                    temp = Int32.Parse(Console.ReadLine());
+                    successfulInput = true;
+                }
+                catch (FormatException)
+                {
+                    Console.Write("You entered a variable of the wrong type. Press any button to repeat...");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+            numberOfEmployees = temp;
+        }
+        static public void InputInformation(out string name, out int salary)
+        {
+            string tempName = "";
+            int tempSalary = 0;
+            bool successfulInput = false;
+            while (successfulInput == false)
+            {
+                try
+                {
+                    Console.WriteLine("Enter the name of employee: ");
+                    tempName = Console.ReadLine();
+                    Console.WriteLine("Enter the salary of employee: ");
+                    tempSalary = Int32.Parse(Console.ReadLine());
+                    successfulInput = true;
+                }
+                catch (FormatException)
+                {
+                    Console.Write("You entered a variable of the wrong type. Press any button to repeat...");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+            name = tempName;
+            salary = tempSalary;
+        }
+    }
+}

93e9f71a18d0eeec7b4563daf4e88505f6ebd913
Scavengere <44781876+Scavengere@users.noreply.github.com>
2019-04-21 15:35:52 +0300

    Add files via upload

---
+++ Task7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the string: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            string text = Console.ReadLine();
+            Console.ResetColor();
+            text = TextTransformer.CheckEnteredString(text);
+            Console.Write($"After transformer: ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(text);
+            Console.ResetColor();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task7/TextTransformer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task7
+{
+    class TextTransformer
+    {
+        public static string CheckEnteredString(string enteredString)
+        {
+            if(string.IsNullOrEmpty(enteredString))
+            {
+                return NullOrEmpty();
+            }
+            else
+            {
+                return ToUpper(enteredString);
+            }
+        }
+        private static string NullOrEmpty()
+        {
+            return "Now string is not empty...";
+        }
+        private static string ToUpper(string enteredString)
+        {
+            string newString = null;
+            foreach (char c in enteredString)
+            {
+                newString = newString + Char.ToUpper(c);
+            }
+            return newString;
+        }
+    }
+}

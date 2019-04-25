daef2b3c71ceca4d20882c610ed090eb82876e41
Scavengere <44781876+Scavengere@users.noreply.github.com>
2019-04-25 21:50:02 +0300

    Add files via upload

---
+++ Task8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter path:");
+            string path = Console.ReadLine();
+            try
+            {
+                WorkingWithFile.ShowAllFilesInDirectory(path);
+                Console.WriteLine("Enter name of file for reading information:");
+                string nameOfFile = Console.ReadLine();
+                WorkingWithFile.ShowFileContent(path, nameOfFile);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task8/WorkingWithFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace Task8
+{
+    class WorkingWithFile
+    {
+        public static void ShowAllFilesInDirectory(string pathOfFile)
+        {
+            List<string> filesName = Directory.GetFiles(pathOfFile).ToList();
+            Console.WriteLine("Files in directory:");
+            foreach (string file in filesName)
+            {
+                Console.WriteLine(file);
+            }
+        }
+
+        public static void ShowFileContent(string pathOfFile, string nameOfFile)
+        {
+            using (StreamReader streamReader = new StreamReader(pathOfFile + nameOfFile))
+            {
+                string line;
+                do
+                {
+                    line = streamReader.ReadLine();
+                    Console.WriteLine(line);
+                }
+                while (line != null) ;
+            }
+        }
+    }
+}

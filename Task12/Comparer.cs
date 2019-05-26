07b7ea6286de72b4d9cc47ab72ba145a407e79be
Scavengere <44781876+Scavengere@users.noreply.github.com>
2019-05-26 22:10:14 +0300

    Add files via upload

---
+++ Task12/Comparer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Task12
+{
+    class Comparer : IComparer<Worker>
+    {
+        public int Compare(Worker x, Worker y)
+        {
+            if (x.WorkPosition > y.WorkPosition)
+                return 1;
+            if (x.WorkPosition < y.WorkPosition)
+                return -1;
+            else return 0;
+        }
+    }
+}

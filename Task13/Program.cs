5e49557a24db7ac133e5d4d5de047614a56fc7d1
Scavengere <44781876+Scavengere@users.noreply.github.com>
2019-05-30 18:37:19 +0300

    Add files via upload

---
+++ Task13/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input email");
+            string email = Console.ReadLine();
+            Console.WriteLine(Validation.ValidateEmail(email));
+            Console.WriteLine("Input url");
+            string url = Console.ReadLine();
+            Console.WriteLine(Validation.ValidateURL(url));
+            Console.WriteLine("Input path");
+            string path = Console.ReadLine();
+            Console.WriteLine(Validation.ValidatFilePath(path));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task13/Validation.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Task13
+{
+    class Validation
+    {
+        public static bool ValidateEmail(string email)
+        {
+            Match match = Regex.Match(email, @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", RegexOptions.IgnoreCase);
+            return match.Success;
+        }
+
+        public static bool ValidateURL(string URL)
+        {
+            Match match = Regex.Match(URL, @"^(http|https|ftp|)\://|[a-zA-Z0-9\-\.]+\.[a-zA-Z](:[a-zA-Z0-9]*)?/?([a-zA-Z0-9\-\._\?\,\'/\\\+&amp;%\$#\=~])*[^\.\,\)\(\s]$", RegexOptions.IgnoreCase);
+            return match.Success;
+        }
+
+        public static bool ValidatFilePath(string path)
+        {
+            Match match = Regex.Match(path, @"^(?:[\w]\:|\\)(\\[a-z_\-\s0-9\.]+)+\.(txt|gif|pdf|doc|docx|xls|xlsx)$", RegexOptions.IgnoreCase);
+            return match.Success;
+        }
+    }
+}

bc3d22cc097b1a30612d6750ae188e628e8bf3af
Scavengere <44781876+Scavengere@users.noreply.github.com>
2019-06-10 15:24:17 +0300

    Add files via upload

---
+++ Task14/Encryption.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+
+namespace Task14
+{
+    class Encryption
+    {
+        private byte[] encrypted;
+        private string publicKey;
+        private string privateKey;
+
+        public Encryption()
+        {
+            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+            {
+                File.WriteAllText("PublicKey.xml", RSA.ToXmlString(false));
+                File.WriteAllText("PrivateKey.xml", RSA.ToXmlString(true));
+            }
+            publicKey = File.ReadAllText("PublicKey.xml");
+            privateKey = File.ReadAllText("PrivateKey.xml");
+        }
+
+        public byte[] Encrypt(byte[] data)
+        {
+            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+            {
+                RSA.FromXmlString(publicKey);
+                encrypted = RSA.Encrypt(data, false);
+            }
+            return encrypted;
+        }
+
+        public byte[] Decrypt(byte[] data)
+        {
+            byte[] decrypted;
+            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+            {
+                RSA.FromXmlString(privateKey);
+                decrypted = RSA.Decrypt(encrypted, false);
+            }
+            return decrypted;
+        }
+
+    }
+}
---
+++ Task14/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace Task14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringToEncrypt = Console.ReadLine();
+            UnicodeEncoding ByteConverter = new UnicodeEncoding();
+            byte[] dataToEncrypt = ByteConverter.GetBytes(stringToEncrypt);
+            Encryption encryption = new Encryption();
+            byte[] encryptedData = encryption.Encrypt(dataToEncrypt);
+            byte[] decryptedData = encryption.Decrypt(encryptedData);
+            Console.WriteLine(ByteConverter.GetString(encryptedData));
+            Console.WriteLine(ByteConverter.GetString(decryptedData));
+            Console.ReadLine();
+        }
+    }
+}

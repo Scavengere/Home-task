71e8c94d795568f082e2e28e6ff8cc26b7930686
Scavengere <44781876+Scavengere@users.noreply.github.com>
2019-06-27 11:38:01 +0300

    Add files via upload

---
+++ Task15/GCD.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task15
+{
+    class GCD
+    {
+        public static int Algorithm1(int number1, int number2)
+        {
+            while (number1 != 0 && number2 != 0)
+                if (number1 > number2)
+                    number1 %= number2;
+                else
+                    number2 %= number1;
+            return number1 + number2;
+        }
+        public static int Algorithm2(int number1, int number2)
+        {
+            if (number2 == 0)
+                return number1;
+            return Algorithm2(number2, number1 % number2);
+        }
+        public static int Algorithm3(int number1, int number2)
+        {
+            while (number1 != number2)
+            {
+                if (number1 > number2)
+                {
+                    int tmp = number1;
+                    number1 = number2;
+                    number2 = tmp;
+                }
+                number2 = number2 - number1;
+            }
+            return number1;
+        }
+
+    }
+}
+
---
+++ Task15/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Task15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            int res;
+            stopwatch.Start();
+            res = GCD.Algorithm1(81, 135);
+            stopwatch.Stop();
+            Console.WriteLine(res);
+            Console.WriteLine(stopwatch.Elapsed);
+
+            stopwatch.Start();
+            res = GCD.Algorithm2(81, 135);
+            stopwatch.Stop();
+            Console.WriteLine(res);
+            Console.WriteLine(stopwatch.Elapsed);
+
+            stopwatch.Start();
+            res = GCD.Algorithm3(81, 135);
+            stopwatch.Stop();
+            Console.WriteLine(res);
+            Console.WriteLine(stopwatch.Elapsed);
+
+            Console.ReadLine();
+        }
+    }
+}

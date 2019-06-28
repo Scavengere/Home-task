f1c0fedb4823c3e5fe44a9a76124f2d241278e5b
Scavengere <44781876+Scavengere@users.noreply.github.com>
2019-06-28 15:55:48 +0300

    Add files via upload

---
+++ Task16/GCD.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Task16
+{
+    class GCD
+    {
+        public static void Algorithm1(int number1, int number2)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            while (number1 != 0 && number2 != 0)
+                if (number1 > number2)
+                    number1 %= number2;
+                else
+                    number2 %= number1;
+            stopwatch.Stop();
+            Console.WriteLine("Algorithm1: result - {0}  time - {1}", number1 + number2, stopwatch.Elapsed);
+        }
+        public static void Algorithm2(int number1, int number2)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int result = _Algorithm2(number1, number2);
+            stopwatch.Stop();
+            Console.WriteLine("Algorithm2: result - {0}  time - {1}", result, stopwatch.Elapsed);
+        }
+        public static int _Algorithm2(int number1, int number2)
+        {
+            if (number2 == 0)
+                return number1;
+            return _Algorithm2(number2, number1 % number2);
+        }
+        public static void Algorithm3(int number1, int number2)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            while (number1 != number2)
+            {
+                if (number1 > number2)
+                {
+                    int tmp = number1;
+                    number1 = number2;
+                    number2 = tmp;
+                }
+                number2 = number2 - number1;
+            }
+            stopwatch.Stop();
+            Console.WriteLine("Algorithm3: result - {0}  time - {1}", number1, stopwatch.Elapsed);
+        }
+    }
+}
---
+++ Task16/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace Task16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread threadAlgorithm2 = new Thread(() => { GCD.Algorithm2(81, 135); });
+            Thread threadAlgorithm3 = new Thread(() => { GCD.Algorithm3(81, 135); });
+            threadAlgorithm2.Start();
+            threadAlgorithm3.Start();
+            GCD.Algorithm1(81, 135);
+            threadAlgorithm2.Join();
+            threadAlgorithm3.Join();
+            Console.ReadKey();
+        }
+    }
+}

ba4c9bd86cee0203be69cb13f82c26a98212e756
Scavengere <44781876+Scavengere@users.noreply.github.com>
2019-07-01 12:06:15 +0300

    Add files via upload

---
+++ Task17/MathematicalOperations.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using MathNet.Numerics.Statistics;
+using System.Linq;
+namespace Task17
+{
+    class MathematicalOperations
+    {
+        public static void Menu(List<double> data)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Your data:");
+                foreach (double number in data)
+                    Console.Write(number + " ");
+                Console.WriteLine("\n");
+                OutoutMenu();
+                int choose = Convert.ToInt16(Console.ReadLine());
+                switch (choose)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine($"Result = {data.Sum()}");
+                            Console.WriteLine("Press any key to continue");
+                            Console.ReadKey();
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine($"Maximum = {data.Maximum()}; Minimum = {data.Minimum()}");
+                            Console.WriteLine("Press any key to continue");
+                            Console.ReadKey();
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine($"Mean = {data.Mean()}");
+                            Console.WriteLine("Press any key to continue");
+                            Console.ReadKey();
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine($"Median = {data.Median()}");
+                            Console.WriteLine("Press any key to continue");
+                            Console.ReadKey();
+                            break;
+                        }
+                    case 5:
+                        {
+                            Console.WriteLine($"Variance = {data.Variance()}");
+                            Console.WriteLine("Press any key to continue");
+                            Console.ReadKey();
+                            break;
+                        }
+                    case 6:
+                        {
+                            Console.WriteLine("Input tau [0,0 - 1,0]");
+                            Console.WriteLine($"Quantile = {data.Quantile(Convert.ToDouble(Console.ReadLine()))}");
+                            Console.WriteLine("Press any key to continue");
+                            Console.ReadKey();
+                            break;
+                        }
+                    case 7:
+                        {
+                            return;
+                        }
+                    default:
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Incorrect input!\nPress any key to repeat");
+                            Console.ResetColor();
+                            Console.ReadKey();
+                            break;
+                        }
+                }
+            }
+        }
+        public static void OutoutMenu()
+        {
+            Console.WriteLine(
+                   "Choose operation:\n" +
+                   "1 Sum\n" +
+                   "2 Maximum and minimum\n" +
+                   "3 Mean\n" +
+                   "4 Median\n" +
+                   "5 Variance\n" +
+                   "6 Quantile\n" +
+                   "7 Exit"
+                );
+        }
+    }
+}
---
+++ Task17/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input size of data");
+            try
+            {
+                int n = Convert.ToInt16(Console.ReadLine());
+                List<double> data = new List<double>();
+                Console.WriteLine("Input data");
+                for (int i = 0; i < n; i++)
+                    data.Add(Convert.ToDouble(Console.ReadLine()));
+
+                MathematicalOperations.Menu(data);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
